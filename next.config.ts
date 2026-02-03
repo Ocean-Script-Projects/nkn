476a1567b8d8e36049cc7c178d03fbbba70575ff
Andrey Kuzmenko <90911508+uncortvpro@users.noreply.github.com>
2026-02-03 18:03:19 +0100

    Update basePath and assetPrefix for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,12 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const repo = 'nkn'; 
-const isGithubPages = !!repo; 
-
 const nextConfig: NextConfig = {
   output: "export",
-
-  basePath: isGithubPages ? `/${repo}` : undefined,
-  assetPrefix: isGithubPages ? `/${repo}/` : undefined,
-
+  basePath: "/nkn",
+  assetPrefix: "/nkn/",
   trailingSlash: true,
-
-  images: { unoptimized: true },
-
-  turbopack: {
-    root: process.cwd(),
-  },
+  images: { unoptimized: true }
 };
-
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
