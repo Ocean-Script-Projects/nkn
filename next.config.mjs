f94000a6dcc34c6073d4db9f5b5cb3068fb8296e
Andrey Kuzmenko <akuzmenko@travanto.de>
2026-02-04 11:46:14 +0100

    Fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
-import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  output: "export",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Убрали output: "export" так как используется middleware и деплой на Heroku
   basePath: "/nkn",
   assetPrefix: "/nkn/",
   trailingSlash: true,
   images: { unoptimized: true },
 };
+
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 export default withNextIntl(nextConfig);
